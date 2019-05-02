e214892eb2e9c28cd6639636b6f1cbe1c35aa6d4
plagesribeiro <45602449+plagesribeiro@users.noreply.github.com>
2019-05-02 19:45:41 -0300

    pares ou impares

--- fase04/ex02/pares.js
+++ fase04/ex02/pares.js
@@ -1,11 +1,6 @@
-function pares(num){
-    var str = num.toString();
-    
-    var n1 = parseInt(str.charAt(0));
-    var n2 = parseInt(str.charAt(1));
-    
+function pares(n1 , n2){    
     var resp = "Os números não são pares";
-    if(n1%2===0 && n2%2===0){
+    if((n1%2===0) && (n2%2===0)){
         resp = "Os números são pares";
     }
     
