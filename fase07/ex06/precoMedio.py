7c2e9bb33bb4e9701b26a596e1e71ea679be07cc
plagesribeiro <45602449+plagesribeiro@users.noreply.github.com>
2019-05-16 20:00:40 -0300

    Create precoMedio.py

---
+++ fase07/ex06/precoMedio.py
@@ -0,0 +1,17 @@
+def precoMedio(*lista):    
+    i=0;
+    soma=0;
+    maiorPreco=lista[0];
+    
+    while (i<len(lista)):
+        if(i!=0):
+            if(maiorPreco['preco'] < lista[i]['preco']):
+                maiorPreco = lista[i];
+        
+        soma = soma + lista[i]['preco'];
+        i=i+1;
+    
+    media = soma/len(lista);
+    
+    print("O produto mais caro se chama "+maiorPreco['nome'])
+    return media;
